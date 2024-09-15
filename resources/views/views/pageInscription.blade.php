27f3f6fa3ffb8440bfe209675d636e889369f725
fchalifour <Infinitrix@outlook.fr>
2024-09-15 17:09:16 -0400

    inscriptionv3

--- resources/views/views/pageInscription.blade.php
+++ resources/views/views/pageInscription.blade.php
@@ -16,16 +16,21 @@
        
         <form method="post" action="" enctype="multipart/form-data">
         @csrf
-        <div class="main-container">
+        <div class="container-xxl">
             <div class="container-xxl">
             <h1>identification</h1>
-                <label for="neq">Numéro d'entreprise du Québec:</label>
-                <input type="text" class="form-control" id="neq" name="neq">
+            <dic class="row">
+                <div class="col-lg-12">
+                    <label for="neq">Numéro d'entreprise du Québec:</label>
+                    <input type="text" class="form-control" id="neq" name="neq">
+                </div>
 
-                <label for="nom_entreprise">Nom de l'entreprise:</label>
-                <input type="text" class="form-control" id="nom_entreprise" name="nom_entreprise" required>
-
-                <div class="container-lg">
+                <div class="col-lg-12">
+                    <label for="nom_entreprise">Nom de l'entreprise:</label>
+                    <input type="text" class="form-control" id="nom_entreprise" name="nom_entreprise" required>
+                </div>
+            </div>
+                <div class="container-xxl">
                     <label for="email">Adresse courriel:</label>
                     <input type="text" class="form-control" id="email" name="email">
 
@@ -35,63 +40,61 @@
                     <label for="mdp2">Ressaisir son mot de passe:</label>
                     <input type="password" class="form-control" id="mdp2" name="mdp2">
                 </div>  
+                
             </div>
             <div class="container-xxl">
             <h1>Coordonnées</h1>
-                <div class="container-lg">
-                <h1>Adresse</h1>
-                    <label for="no_rue">No. civique:</label>
-                    <input type="text" class="form-control" id="no_rue" name="no_rue">
+                <div class="container-xxl">
+                    <h1>Adresse</h1>
+                    <dic class="row">
 
-                    <label for="rue">Rue:</label>
-                    <input type="text" class="form-control" id="rue" name="rue">
+                        <div class="col-sm-3">
+                            <label for="no_rue">No. civique:</label>
+                            <input type="text" class="form-control" id="no_rue" name="no_rue">
+                        </div>
 
-                    <label for="no_bureau">Bureau:</label>
-                    <input type="text" class="form-control" id="no_bureau" name="no_bureau">
+                        <div class="col-sm-6">
+                            <label for="rue">Rue:</label>
+                            <input type="text" class="form-control" id="rue" name="rue">
+                        </div>
 
-                    <label for="ville">Ville:</label>
-                    <input type="text" class="form-control" id="ville" name="ville">
+                        <div class="col-sm-3">
+                            <label for="no_bureau">Bureau:</label>
+                            <input type="text" class="form-control" id="no_bureau" name="no_bureau">
+                        </div>
 
-                    <label for="province">Province:</label>
-                    <input class="form-control" list="provinces" name="province" id="province">
-                    <datalist id="provinces">
-                        <option value="Alberta">
-                        <option value="Colombie-Britannique">
-                        <option value="Île-du-Prince-Édouard">
-                        <option value="Manitoba">
-                        <option value="Nouveau-Brunswick">
-                        <option value="Nouvelle-Écosse">
-                        <option value="Ontario">
-                        <option value="Québec">
-                        <option value="Saskatchewan">
-                        <option value="Terre-Neuve-et-Labrador">
-                        <option value="Territoires du Nord-Ouest">
-                        <option value="Nunavut">
-                        <option value="Yukon">
-                    </datalist>
+                        <div class="col-lg-12">
+                            <label for="ville">Ville:</label>
+                            <input type="text" class="form-control" id="ville" name="ville">
+                        </div>
 
-                    <label for="code_postal">Code postal:</label>
-                    <input type="text" class="form-control" id="code_postal" name="code_postal">
+                        <div class="col-sm-9">
+                            <label for="province">Province:</label>
+                            <input class="form-control" list="provinces" name="province" id="province">
+                            <datalist id="provinces">
+                                <option value="Alberta">
+                                <option value="Colombie-Britannique">
+                                <option value="Île-du-Prince-Édouard">
+                                <option value="Manitoba">
+                                <option value="Nouveau-Brunswick">
+                                <option value="Nouvelle-Écosse">
+                                <option value="Ontario">
+                                <option value="Québec">
+                                <option value="Saskatchewan">
+                                <option value="Terre-Neuve-et-Labrador">
+                                <option value="Territoires du Nord-Ouest">
+                                <option value="Nunavut">
+                                <option value="Yukon">
+                            </datalist>
+                        </div>
 
-                    
-                </div>  
+                        <div class="col-sm-3">
+                            <label for="code_postal">Code postal:</label>
+                            <input type="text" class="form-control" id="code_postal" name="code_postal">
+                        </div>
 
-                <label for="neq">Numéro d'entreprise du Québec:</label>
-                <input type="text" class="form-control" id="neq" name="neq">
-
-                <label for="nom_entreprise">Nom de l'entreprise:</label>
-                <input type="text" class="form-control" id="nom_entreprise" name="nom_entreprise" required>
-
-                <div class="container-lg">
-                    <label for="email">Adresse courriel:</label>
-                    <input type="text" class="form-control" id="email" name="email">
-
-                    <label for="mdp">Choisir son mot de passe:</label>
-                    <input type="password" class="form-control" id="mdp" name="mdp" required>
-
-                    <label for="mdp2">Ressaisir son mot de passe:</label>
-                    <input type="password" class="form-control" id="mdp2" name="mdp2">
-                </div>  
+                    </div>   
+                </div> 
             </div>
         </div>
         
