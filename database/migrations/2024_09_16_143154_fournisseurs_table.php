d1dc64391b71bc8202b8ef44c783792107d54145
fchalifour <93669813+Infinitrix10018@users.noreply.github.com>
2024-09-16 10:32:56 -0400

    bd v2

---
+++ database/migrations/2024_09_16_143154_fournisseurs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('NEQ')->nullable();
+            $table->string('nom_entreprise', lenght: 64)->required();
+            $table->string('email', lenght: 64);
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
