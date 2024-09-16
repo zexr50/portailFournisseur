743c902eef1b37b31525d126bd12289074a1f05f
TheWiseToad <143433011+zexr50@users.noreply.github.com>
2024-09-16 09:35:15 -0400

    slideShow in progress

--- resources/views/views/pageAccueil.blade.php
+++ resources/views/views/pageAccueil.blade.php
@@ -3,24 +3,25 @@
     @section('css')
         <link rel="stylesheet" href="{{ asset('css/pageAccueil.css') }}">
     @show
-    @section('js')
-        <script src=""></script>
-    @endsection
+    
+    
     @section('content')
 
     <h1>Votre Portail Fournisseur</h1>
     <div>
-        <button type="submit" class="button" id="buttonLeftSS" onclick="buttonLeftSS()"><</button>
+        <button type="submit" class="button" id="buttonLeftSS" onclick="showPrevSlide()"><</button>
         
-        <div>
-            <img src="{{ asset('img/user.svg') }}" alt="Utilisateur" style="">
-            <img src="{{ asset('img/moon.svg') }}" alt="Lune" style="display:none;">
-            <img src="{{ asset('img/sun.svg') }}" alt="Soleil" style="display:none;">
+        <div id="slideShow">
+            <img src="{{ asset('img/user.svg') }}" alt="Utilisateur" id="slide1" style="">
+            <img src="{{ asset('img/moon.svg') }}" alt="Lune"        id="slide2" style="display:none;">
+            <img src="{{ asset('img/sun.svg') }}"  alt="Soleil"      id="slide3" style="display:none;">
         </div>
 
-        <button type="button" class="button" id="buttonRightSS" onclick="buttonRightSS()">></button>
+        <button type="button" class="button" id="buttonRightSS" onclick="showNextSlide()">></button>
     </div>
 
-</div>
-
+    
+    @section('js')
+        <script src="{{ asset('js/slideShow.js') }}"></script>
+    @endsection
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 });
 */
 
-Route::get('/', function () {return view('views/index');}); //dumb page
-Route::get('/Accueil', function () {return view('views/pageAccueil');});
+Route::get('/', function () {return view('views/index');}); //dump page
+Route::get('/Accueil', function () {return view('views/pageAccueil');})->name("Accueil");
 Route::get('/ConnexionFournisseurCourriel', function () {return view('views/pageConnexionFournisseurCourriel');});
 Route::get('/ConnexionFournisseurNEQ', function () {return view('views/pageConnexionFournisseurNEQ');});
 Route::get('/inscription', function () {return view('views/pageInscription');});
