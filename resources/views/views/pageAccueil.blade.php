1b17f1e540d3c6e6c07d62a6687cc3faa483fd2b
TheWiseToad <143433011+zexr50@users.noreply.github.com>
2024-09-18 15:48:59 -0400

    Acceuil Fini
    still needs bug fixes and a few changes

--- resources/views/views/pageAccueil.blade.php
+++ resources/views/views/pageAccueil.blade.php
@@ -3,7 +3,7 @@
     @section('css')
         <link rel="stylesheet" href="{{ asset('css/pageAccueil.css') }}">
     @show
-    
+    <!-- Needs to be adapted for bigger and smaller screens !-->
     
     @section('content')
     <h1>Votre Portail Fournisseur</h1>
@@ -14,6 +14,12 @@
             <img src="{{ asset('img/stockImg1.jpg') }}" alt="img1" id="slide1" class="slide" style="opacity: 1;">
             <img src="{{ asset('img/stockImg2.jpg') }}" alt="img2" id="slide2" class="slide" style="opacity: 0;">
             <img src="{{ asset('img/stockImg3.jpg') }}" alt="img3" id="slide3" class="slide" style="opacity: 0;">
+        </div>
+
+        <div id="slideShowText">
+            <h3 class="text" style="opacity: 1;">Hello this is a test</h3>
+            <h3 class="text" style="opacity: 0;">Hello this is working</h3>
+            <h3 class="text" style="opacity: 0;">Lets a go itsa me mario</h3>
         </div>
 
         <button type="button" class="button" id="buttonRight" onclick="showNextSlide()">></button>
