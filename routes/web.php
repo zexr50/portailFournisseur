5907152eaa9683a7dd4dedd90bf13541ab4442fb
JonathanGremmoGit <117290338+JonathanGremmoGit@users.noreply.github.com>
2024-09-23 10:26:54 -0400

    e

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,7 @@ use App\Http\Controllers\InscriptionController;
 
 Route::get('/', function () {return view('views/index');}); //dump page
 Route::get('/Accueil', function () {return view('views/pageAccueil');})->name("Accueil");
-Route::get('/ConnexionFournisseurCourriel', function () {return view('views/pageConnexionFournisseurCourriel');})->name("ConnexionFournisseurCourriel");
-Route::get('/ConnexionFournisseurNEQ', function () {return view('views/pageConnexionFournisseurNEQ');})->name("ConnexionFournisseurNEQ");
+Route::get('/inscription', function () {return view('views/pageInscription');})->name("Inscription");
 Route::get('/pageInscriptionsLicences', function () {return view('views/pageInscriptionsLicences');});
 Route::get('/MenuFournisseur', function () {return view('views/pageMenuFournisseur');})->name("MenuFournisseur");
 Route::get('/AjoutFinances', function () {return view('views/pageAjoutFinancesFournisseur');})->name("AjoutFinances");
