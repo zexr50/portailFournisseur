66a46acfd23d1e2c9cf5262658ed8537f2b2305f
fchalifour <93669813+Infinitrix10018@users.noreply.github.com>
2024-10-21 10:06:38 -0400

    modif migrate user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('activer')->default(true);
+            $table->string('role')->default('fournisseur');
             $table->rememberToken();
             $table->timestamps();
         });
