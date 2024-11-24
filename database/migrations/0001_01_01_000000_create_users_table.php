4681bfdddb5c97910af72670427e80bf2826fb10
fchalifour <93669813+Infinitrix10018@users.noreply.github.com>
2024-11-24 09:27:22 -0500

    modif dans migrattion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('id_fournisseurs');
             $table->string('name');
-            $table->string('email')->unique()->nullable();
+            $table->string('email')->nullable();
             $table->string('NEQ')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
